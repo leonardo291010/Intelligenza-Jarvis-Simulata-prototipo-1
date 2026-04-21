1403c927c2e5f7cecbe7a0353940dfc06f9f3a99
leonardo291010 <leopoldobrizzi@gmail.com>
2026-04-21 20:40:28 +0200

    Update speech.js

--- speech.js
+++ speech.js
@@ -1,1 +1,9 @@
+export function speak(text) {
+  const speech = new SpeechSynthesisUtterance(text);
+  speech.lang = "it-IT";
+  speech.rate = 1;
+  speech.pitch = 1;
+  speech.volume = 1;
 
+  speechSynthesis.speak(speech);
+}
