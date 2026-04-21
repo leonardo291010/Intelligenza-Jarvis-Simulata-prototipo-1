4365af10f57f9be3a08a7f688ce894e9f4968368
leonardo291010 <leopoldobrizzi@gmail.com>
2026-04-21 20:40:12 +0200

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,1 +1,12 @@
+const camera = document.getElementById("camera");
+const preview = document.getElementById("preview");
 
+export function initCamera() {
+  camera.addEventListener("change", () => {
+    const file = camera.files[0];
+    if (!file) return;
+
+    const url = URL.createObjectURL(file);
+    preview.src = url;
+  });
+}
